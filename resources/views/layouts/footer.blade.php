3f31d4c1eb9100f62f2dddfd125be0936152bcff
Matteo Cirami <mcirami@users.noreply.github.com>
2025-03-18 16:55:05 -0500

    adjusting footer menu styles

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="w-full py-20 text-center relative">
     <div class="slanted_div alt z-1"></div>
-    <div class="container mx-auto px-5 z-2 relative">
+    <div class="container mx-auto px-2 md:px-5 z-2 relative">
         <div class="logo mx-auto max-w-60 md:max-w-80 flex justify-center items-center">
             <a href="/">
                 <img src="{{ asset('images/logo.png') }}" alt="">
             </a>
         </div>
-        <div class="footer_nav flex justify-center items-center mt-10 mb-5 gap-5 md:gap-10 text-white">
+        <div class="footer_nav flex justify-center items-center flex-wrap mt-10 mb-5 gap-5 md:gap-10 text-white">
             <a href="/">Home</a>
             <a href="/about">About</a>
             <a href="/daily-plan">Daily Plan</a>
